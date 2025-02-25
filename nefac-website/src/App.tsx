af88e1a0b0d1868eef5914074fa02ac9748acfc8
bryanbab <bryanbaboolal@gmail.com>
2025-02-25 17:39:22 -0500

    Add leadership page and executive committee section

--- nefac-website/src/App.tsx
+++ nefac-website/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Bubble from "./app/components/Bubble";
 import "./globals.css";
 import SustainingMembershipsPage from "./app/pages/sustaining-memberships/page";
+import LeadershipPage from "./app/pages/leadership/page";
 import NewsPage from "./app/pages/nefac-news/page";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./app/components/header";
@@ -15,6 +16,7 @@ const App = () => {
       <Routes>
         <Route path="/nefac-news" element={<NewsPage />} />
         <Route path="/sustaining-memberships" element={<SustainingMembershipsPage />} />
+         <Route path="/leadership" element={<LeadershipPage />} />
       </Routes>
     </Router>
     </>
