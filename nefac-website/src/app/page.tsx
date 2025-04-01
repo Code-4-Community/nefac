0204a55533cb188aabc2cc431e20c7b762c2642d
piersolh <145268163+piersolh@users.noreply.github.com>
2025-04-01 16:06:08 -0400

    didn't stage file correctly earlier, this should include all of the changes made

--- nefac-website/src/app/page.tsx
+++ nefac-website/src/app/page.tsx
@@ -1,11 +1,12 @@
 import CommentarySection from "../app/components/CommentarySection";
-
+import Footer from "../app/components/footer"
 export default function Home() {
   return (
     <div className="px-36 pt-12 flex flex-row flex-wrap gap-4">
-      <div className="z-0">
+      <div className="z-0 pb-20">
         <CommentarySection />
       </div>
+      <Footer />
     </div>
   );
 }
