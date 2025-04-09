91144b2446d4c7c3b41a1ae4407f3a0d05bdb2f7
Troy Gunawardene <troy.hiruka@gmail.com>
2025-04-09 13:10:04 -0400

    generalize carousels, implement community & advocacy

--- nefac-website/src/app/page.tsx
+++ nefac-website/src/app/page.tsx
@@ -1,12 +1,15 @@
-import CommentarySection from "../app/components/CommentarySection";
+import AdvocacySection from "./components/AdvocacySection";
+import CommentarySection from "./components/CommentarySection";
+import CommunitySection from "./components/CommunitySection";
 
 export default function Home() {
   return (
     <div className="px-36 pt-12 flex flex-row flex-wrap gap-4">
       <div className="z-0 pb-20">
         <CommentarySection />
+        <CommunitySection />
+        <AdvocacySection />
       </div>
     </div>
   );
 }
-
