ae1ce5ec03aac3590879e027667519c4a9e09275
amywng <147568742+amywng@users.noreply.github.com>
2025-04-13 12:33:21 -0400

    nefac news api integration wip

--- nefac-website/src/app/components/news-bubble.tsx
+++ nefac-website/src/app/components/news-bubble.tsx
@@ -13,17 +13,19 @@ export const NewsBubble = ({ image, title, link, date, header, headerPreview }: 
     if (header) {
         return (
             <div className="flex flex-row gap-6 mb-6">
-                <a href={link} className="w-3/4">
-                    <div className="rounded-xl h-full bg-[linear-gradient(to_top,rgba(0,0,0,0.65),rgba(0,0,0,0.0)),url('/images/filler.jpeg')]
-                                    bg-cover bg-no-repeat bg-center min-h-[400px]">
-                    </div>
-                    <div className="relative">
-                        <div className="absolute flex flex-col bottom-0 text-white font-bold text-2xl bg-rounded-md p-6 w-2/3">
+                <a href={link} className="w-3/4 relative">
+                    <div 
+                        className="rounded-xl h-full min-h-[400px] bg-cover bg-no-repeat bg-center"
+                        style={{ 
+                            backgroundImage: `linear-gradient(to top, rgba(0,0,0,0.65), rgba(0,0,0,0.0)), url(${image})` 
+                        }}
+                    >
+                        <div className="absolute flex flex-col bottom-0 text-white font-bold text-2xl p-6 w-2/3">
                             {title && (
                                 <h2 
-                                    className={`mb-2 font-extrabold
-                                    `}>{title ?? "Title"}
-                                </h2>
+                                    className="mb-2 font-extrabold"
+                                    dangerouslySetInnerHTML={{ __html: title ?? "Title" }}
+                                />
                             )}
                             {date && (
                                 <p className="font-normal text-[#F8F8F8] text-xs">{date}</p>
@@ -32,7 +34,12 @@ export const NewsBubble = ({ image, title, link, date, header, headerPreview }: 
                     </div>
                 </a>
                 <div className="w-1/4 flex flex-col">
-                    <p className="text-base text-[#444444] leading-8 mb-4">{headerPreview}</p>
+                    {headerPreview && headerPreview.split(' ').length > 0 ? (
+                        <p className="text-base text-[#444444] leading-8 mb-4">
+                            <span className="text-3xl [&>p]:inline" dangerouslySetInnerHTML={{ __html: headerPreview.split(' ')[0] }} />
+                            <span dangerouslySetInnerHTML={{ __html: ' ' + headerPreview.split(' ').slice(1).join(' ') }} />
+                        </p>
+                    ) : null}
                     <a href={link} className="bg-nefacblue text-white rounded-md p-2 text-center font-semibold">
                         <p>Read More</p>
                     </a>
@@ -50,17 +57,17 @@ export const NewsBubble = ({ image, title, link, date, header, headerPreview }: 
                         className="rounded-xl h-[180px] w-full object-cover mb-4"
                     />
                 )}
-                <div className="">
-                    {title && (
-                        <h2 
-                            className="mb-2 font-semibold mb-4">{title ?? "Title"}
-                        </h2>
-                    )}
-                    {date && (
-                        <p className="font-normal text-[#949494] text-xs">{date}</p>
-                    )}
+                    <div className="">
+                        {title && (
+                            <h2 
+                                className="mb-2 font-semibold mb-4">{title ?? "Title"}
+                            </h2>
+                        )}
+                        {date && (
+                            <p className="font-normal text-[#949494] text-xs">{date}</p>
+                        )}
+                    </div>
                 </div>
-            </div>
             </a>
         )
     }
