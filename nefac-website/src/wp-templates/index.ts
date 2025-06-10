c776a0c0017a7000bd3efc840de181d0f8f6c3f0
Dalton Burkhart <daltonburkhart.75@gmail.com>
2025-06-10 01:35:06 -0400

    Updated commit for Troy

--- nefac-website/src/wp-templates/index.ts
+++ nefac-website/src/wp-templates/index.ts
@@ -1,9 +1,7 @@
-import LeadershipTemplate from "./leadership";
 import SingleTemplate from "./single";
  
 const templates = {
 	single: SingleTemplate,
-	leadership: LeadershipTemplate,
 };
  
 export default templates;
