bdbeb12d312c82c34e1281432a0ea367133864e8
lyannne <xu.ly@northeastern.edu>
2025-06-15 23:13:59 -0400

    final commit frfr

--- nefac-website/src/components/home-page/WhatIsNefacSection.tsx
+++ nefac-website/src/components/home-page/WhatIsNefacSection.tsx
@@ -56,6 +56,7 @@ export function mapLinks(links: Link[]) {
     </ul>)
 }
 
+// Main Section Component
 export default function WhatIsNefacSection() {
   return (
     <section className="overflow-x-hidden mb-16 px-20">
