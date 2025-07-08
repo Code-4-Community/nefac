9dd968175da0502449ab904a548bb2c6c52d9a63
Dalton Burkhart <daltonburkhart.75@gmail.com>
2025-07-08 00:28:24 -0400

    Abstracted all logic in education initiatvie to sidebar wrapper to create similar advocacy page

---
+++ nefac-website/src/components/advocacy/AdvocacyInitiativeWrapper.tsx
@@ -0,0 +1,22 @@
+import SidebarWrapper from "../sidebar/SidebarWrapper";
+
+interface AdvocacyeInitiativeWrapperProps {
+  children?: React.ReactNode;
+}
+
+export default function AdvocacyInitiativeWrapper({ children }: AdvocacyeInitiativeWrapperProps) {
+  return (
+    <SidebarWrapper
+      title="Advocacy"
+      items={[
+        { title: "NEFAC Report (spotify link)", link: "https://rss.com/podcasts/nefacreport/" },
+        { title: "Commentary and Coverage", link: "/advocacy/commentary-and-coverage" },
+        { title: "Statements and Letters", link: "/advocacy/statements-and-letters" },
+        { title: "New England First Amendment Awards", link: "/advocacy/new-england-first-amendment-awards" },
+        { title: "Sunshine Week", link: "/advocacy/sunshine-week" },
+      ]}
+    >
+      {children}
+    </SidebarWrapper>
+  );
+}
---
+++ nefac-website/src/pages/advocacy/demo.tsx
@@ -0,0 +1,16 @@
+import AdvocacyInitiativeWrapper from "@/components/advocacy/AdvocacyInitiativeWrapper";
+
+const AdvocacyPage: React.FC = () => {
+    return (
+        <AdvocacyInitiativeWrapper>
+            <div>
+                Testing Advocacy Initiative Wrapper
+            </div>
+            <p>
+                Demo Page
+            </p>
+        </AdvocacyInitiativeWrapper>
+    )
+};
+
+export default AdvocacyPage;
