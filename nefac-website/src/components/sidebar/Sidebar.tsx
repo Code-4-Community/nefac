abf5f7db5a25e1f4b95906324629e9ba5953f1a3
builderpepc <troy.hiruka@gmail.com>
2025-07-08 12:13:55 -0400

    fix icon related error

--- nefac-website/src/components/sidebar/Sidebar.tsx
+++ nefac-website/src/components/sidebar/Sidebar.tsx
@@ -68,7 +68,7 @@ export default function Sidebar({ items }: SidebarProps) {
           onClick={openSidebar}
           className="fixed bottom-5 left-0 z-50 bg-gray-200 w-[60px] h-[60px] text-2xl font-bold rounded-r-md shadow-md flex items-center justify-center md:hidden"
         >
-          <FontAwesomeIcon icon={faAngleRight} />
+          <FontAwesomeIcon icon={faAngleRight} className="" />
         </button>
       )}
 
@@ -85,7 +85,7 @@ export default function Sidebar({ items }: SidebarProps) {
             className="fixed top-5 right-5 w-[60px] h-[60px] 
               text-2xl font-bold flex items-center justify-center"
           >
-            <FontAwesomeIcon icon={faTimes} />
+            <FontAwesomeIcon icon={faTimes} className="" />
           </button>
 
           <div className="flex flex-col gap-5 mt-20">
