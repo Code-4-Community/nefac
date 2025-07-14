8adece6b75558ba7435a022ff30d1b8ef88e0d25
builderpepc <troy.hiruka@gmail.com>
2025-07-13 23:59:36 -0400

    add wp-env configuration

---
+++ tools/wp-env/imports/env-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! -f /var/www/html/.initialized ]; then
+  # one-time setup commands here
+
+  echo "Setting up NEFAC WordPress development environment..."
+
+  # import from version-controlled database copy
+  wp db import ./imports/database.sql
+
+  # fix installation of wp-graphql-content-blocks
+  (cd wp-content/plugins/wp-graphql-content-blocks && composer install)
+
+  # fix possibly broken permalink behavior
+  wp rewrite flush --hard
+
+  echo "Finished setting up"
+
+  touch /var/www/html/.initialized
+fi
