a9bc79acb25eca4d9585a9c208268c4bd035521d
Dalton Burkhart <daltonburkhart.75@gmail.com>
2025-07-18 19:02:18 -0400

    Updated block editor UI

--- nefac-website/wp-blocks/Person/edit.tsx
+++ nefac-website/wp-blocks/Person/edit.tsx
@@ -1,35 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useBlockProps } from '@wordpress/block-editor';
 import { TextControl } from '@wordpress/components';
 import { BlockEditProps } from '@wordpress/blocks';
 import { PersonAttributes } from './PersonAttributes';
+import { Icon } from '@wordpress/icons';
+import { chevronDown, chevronRight } from '@wordpress/icons';
 
-// UI in the Gutenberg block editor
 function Edit({ attributes, setAttributes }: BlockEditProps<PersonAttributes>) {
   const { name = '', role = '' } = attributes;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
     <div
       {...useBlockProps({
         className: 'person-block',
         style: {
-          padding: '1rem',
+          padding: '0.5rem 0.75rem',
           border: '1px solid #ccc',
-          marginBottom: '1rem',
+          marginBottom: '0.5rem',
           borderRadius: '6px',
         },
       })}
     >
-      <TextControl
-        label="Name"
-        value={name}
-        onChange={(value) => setAttributes({ name: value })}
-      />
-      <TextControl
-        label="Role"
-        value={role}
-        onChange={(value) => setAttributes({ role: value })}
-      />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          cursor: 'pointer',
+          fontSize: '16px',
+        }}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <p>
+          {name} {role ? ` • ${role}` : ''}
+        </p>
+        <Icon icon={isOpen ? chevronDown : chevronRight} />
+      </div>
+
+      {isOpen && (
+        <div style={{ marginTop: '1rem' }}>
+          <TextControl
+            label="Name"
+            value={name}
+            onChange={(value) => setAttributes({ name: value })}
+          />
+          <TextControl
+            label="Role"
+            value={role}
+            onChange={(value) => setAttributes({ role: value })}
+          />
+        </div>
+      )}
     </div>
   );
 }
