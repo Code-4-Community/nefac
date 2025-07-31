6387f17aec3bf8471d9606d1f22d4993c7bcc732
yurikakan <kan.yu@northeastern.edu>
2025-07-31 04:00:01 -0700

    fixed width bug in search bar

--- nefac-website/src/components/header/search-bar.tsx
+++ nefac-website/src/components/header/search-bar.tsx
@@ -1,17 +1,17 @@
 import { Input } from "@/components/ui/input";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEllipsisV, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faTimes, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const SearchBar = () => {
   return (
-    <div className="flex items-center w-[399px] h-[36px] space-x-2 rounded-lg px-3.5 py-2 text-gray-800 border border-gray-300">
+    <div className="flex items-center w-full max-w-[399px] h-[36px] space-x-2 rounded-lg px-3.5 py-2 text-gray-600 border border-gray-300">
       <FontAwesomeIcon icon={faSearch} className="h-4 w-4" />
       <Input
         type="search"
         placeholder="Search"
         className="w-full border-0 h-8"
       />
-      <FontAwesomeIcon icon={faEllipsisV} className="h-4 w-4" />
+      <FontAwesomeIcon icon={faTimes} className="h-4 w-4" />
     </div>
   );
 };
